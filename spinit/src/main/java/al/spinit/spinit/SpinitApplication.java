51579e103d23b5ef1aab2f40cf9faa6ee060e820
armirigj <armirgjini76@gmail.com>
2025-09-21 19:40:29 +0200

    Initialization of the Spin It project

---
+++ spinit/src/main/java/al/spinit/spinit/SpinitApplication.java
@@ -0,0 +1,13 @@
+package al.spinit.spinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinitApplication.class, args);
+	}
+
+}
